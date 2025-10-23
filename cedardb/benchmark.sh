a3e5e81af01ec239434f9548878fa90f211799c7
Lukas Vogel <lukas@cedardb.com>
2025-10-23 11:00:20 +0200

    use CedarDB's native binary instead of docker

--- cedardb/benchmark.sh
+++ cedardb/benchmark.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo apt-get install -y docker.io postgresql-client gzip
+sudo apt-get install -y postgresql-client gzip
 
 #Download the dataset
 wget --no-verbose --continue 'https://rtadatasets.timescale.com/customers.csv.gz'
@@ -15,27 +15,32 @@ chmod 777 customers.csv products.csv orders.csv order_items.csv order_events.csv
 mkdir -p dataset
 mv *.csv dataset/
 
+echo "Installing CedarDB..."
+curl https://get.cedardb.com > install.sh
+chmod +x install.sh
+./install.sh -y # non-interactive install
+./cedar/cedardb --createdb db &
 
-# get and configure CedarDB image
-echo "Starting CedarDB..."
-docker run --rm -p 5432:5432 -v ./dataset:/dataset -v ./db:/var/lib/cedardb/data -e CEDAR_PASSWORD=test --name cedardb cedardb/cedardb:latest > /dev/null 2>&1 &
+# wait for cedardb to start
+until pg_isready -h localhost -U postgres > /dev/null 2>&1; do sleep 1; done
 
-# wait for container to start
-until pg_isready -h localhost --dbname postgres -U postgres > /dev/null 2>&1; do sleep 1; done
+# set the password for the postgres user via domain socket
+psql -h /tmp -U postgres -c "ALTER USER postgres with password 'test';"
 
-
-PGPASSWORD=test psql -h localhost -U postgres -c "CREATE DATABASE test"
+# We can now login with password
+export PGPASSWORD=test
+psql -h localhost -U postgres -c "CREATE DATABASE test"
 
 # Import the data
-PGPASSWORD=test psql -h localhost -U postgres --dbname=test < create.sql #import
+psql -h localhost -U postgres --dbname=test < create.sql #import
 
-PGPASSWORD=test psql -h localhost -U postgres --dbname=test -t -c '\timing' -c "\\COPY customers FROM 'dataset/customers.csv' WITH (FORMAT csv);" #import
-PGPASSWORD=test psql -h localhost -U postgres --dbname=test -t -c '\timing' -c "\\COPY products FROM 'dataset/products.csv' WITH (FORMAT csv);" #import
-PGPASSWORD=test psql -h localhost -U postgres --dbname=test -t -c '\timing' -c "\\COPY orders FROM 'dataset/orders.csv' WITH (FORMAT csv);" #import
-PGPASSWORD=test psql -h localhost -U postgres --dbname=test -t -c '\timing' -c "\\COPY order_items FROM 'dataset/order_items.csv' WITH (FORMAT csv);" #import
-PGPASSWORD=test psql -h localhost -U postgres --dbname=test -t -c '\timing' -c "\\COPY order_events FROM 'dataset/order_events.csv' WITH (FORMAT csv);" #import
+psql -h localhost -U postgres --dbname=test -t -c '\timing' -c "\\COPY customers FROM 'dataset/customers.csv' WITH (FORMAT csv);" #import
+psql -h localhost -U postgres --dbname=test -t -c '\timing' -c "\\COPY products FROM 'dataset/products.csv' WITH (FORMAT csv);" #import
+psql -h localhost -U postgres --dbname=test -t -c '\timing' -c "\\COPY orders FROM 'dataset/orders.csv' WITH (FORMAT csv);" #import
+psql -h localhost -U postgres --dbname=test -t -c '\timing' -c "\\COPY order_items FROM 'dataset/order_items.csv' WITH (FORMAT csv);" #import
+psql -h localhost -U postgres --dbname=test -t -c '\timing' -c "\\COPY order_events FROM 'dataset/order_events.csv' WITH (FORMAT csv);" #import
 
-PGPASSWORD=test psql -h localhost -U postgres --dbname=test -c "\t" -c "SELECT pg_total_relation_size('order_events') + pg_total_relation_size('orders') + pg_total_relation_size('order_items') + pg_total_relation_size('products') + pg_total_relation_size('customers');" #datasize
+psql -h localhost -U postgres --dbname=test -c "\t" -c "SELECT pg_total_relation_size('order_events') + pg_total_relation_size('orders') + pg_total_relation_size('order_items') + pg_total_relation_size('products') + pg_total_relation_size('customers');" #datasize
 
 ./run.sh 2>&1 | tee log.txt
 
@@ -45,3 +50,4 @@ cat log.txt | grep -oP 'Time: \d+\.\d+ ms' | sed -r -e 's/Time: ([0-9]+\.[0-9]+)
 echo "General Purpose" #tag
 echo "Real-time Analytics" #tag
 echo "CedarDB" #name
+pkill cedardb
